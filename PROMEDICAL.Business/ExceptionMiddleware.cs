d3d2c6bd7b573972ab1aa53da2d93481267e9d10
Jonatan Fajardo <69583436+JonatanFajardo@users.noreply.github.com>
2025-02-28 22:24:01 -0600

    Correccion de problemas.

--- PROMEDICAL.Business/ExceptionMiddleware.cs
+++ PROMEDICAL.Business/ExceptionMiddleware.cs
@@ -8,41 +8,41 @@ using System.Threading.Tasks;
 
 namespace PROMEDICAL.Business
 {
-    public class ExceptionMiddleware
-    {
-        private readonly RequestDelegate _next;
-        private readonly ILogger _logger;
-        public ExceptionMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
-        {
-            _next = next;
-            _logger = loggerFactory.CreateLogger<ExceptionMiddleware>();
-        }
-        public async Task InvokeAsync(HttpContext httpContext)
-        {
-            transaction
-            try
-            {
-                await _next(httpContext);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, $"Oooops!Algo salió mal:sass");
-                await InternalServerErrorHandleGlobalExceptionAsync(httpContext, ex);
-            }
-        }
+	public class ExceptionMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private readonly ILogger _logger;
+		public ExceptionMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+		{
+			_next = next;
+			_logger = loggerFactory.CreateLogger<ExceptionMiddleware>();
+		}
+		public async Task InvokeAsync(HttpContext httpContext)
+		{
+			//transaction
+			try
+			{
+				await _next(httpContext);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, $"Oooops!Algo salió mal:sass");
+				await InternalServerErrorHandleGlobalExceptionAsync(httpContext, ex);
+			}
+		}
 
-        private static Task InternalServerErrorHandleGlobalExceptionAsync(HttpContext context, Exception exception)
-        {
-            context.Response.ContentType = "application/json";
-            //if (context.sta)
-            //{
+		private static Task InternalServerErrorHandleGlobalExceptionAsync(HttpContext context, Exception exception)
+		{
+			context.Response.ContentType = "application/json";
+			//if (context.sta)
+			//{
 
-            //}
-            ServiceResult serviceResult = new ServiceResult();
-            serviceResult.Error();
+			//}
+			ServiceResult serviceResult = new ServiceResult();
+			serviceResult.Error();
 
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            return context.Response.WriteAsync(JsonConvert.SerializeObject(serviceResult));
-        }
-    }
+			context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+			return context.Response.WriteAsync(JsonConvert.SerializeObject(serviceResult));
+		}
+	}
 }
--- PROMEDICAL.DataAccess/Extensions/Transaction.cs
+++ PROMEDICAL.DataAccess/Extensions/Transaction.cs
@@ -7,115 +7,115 @@ using System.Threading.Tasks;
 
 namespace PROMEDICAL.DataAccess.Extensions
 {
-    class Transaction
-    {
-        public static async Task<IEnumerable<T>> SelectAsync<T>(string sqlQuery)
-        {
-            using (var database = new SqlConnection(AppPromedicalDbContext.ConnectionString))
-            {
-                var result = await database.QueryAsync<T>(sqlQuery, commandType: CommandType.StoredProcedure);
-                //if (result == null && result.Count() > 0)
-                database.Close();
-                database.Dispose();
-                if (Affected(result))
-                    return null;
+	public class Transaction
+	{
+		public static async Task<IEnumerable<T>> SelectAsync<T>(string sqlQuery)
+		{
+			using (var database = new SqlConnection(AppPromedicalDbContext.ConnectionString))
+			{
+				var result = await database.QueryAsync<T>(sqlQuery, commandType: CommandType.StoredProcedure);
+				//if (result == null && result.Count() > 0)
+				database.Close();
+				database.Dispose();
+				if (Affected(result))
+					return null;
 
-                return result;
-            }
-        }
+				return result;
+			}
+		}
 
-        public static async Task<IEnumerable<T>> SelectAsync<T>(string sqlQuery, DynamicParameters parameters)
-        {
-            using (var database = new SqlConnection(AppPromedicalDbContext.ConnectionString))
-            {
-                database.Open();
-                var result = await database.QueryAsync<T>(sqlQuery, parameters, commandType: CommandType.StoredProcedure);
-                database.Close();
-                database.Dispose();
-                //if (result == null && result.Count() > 0)
-                if (Affected(result))
-                    return null;
+		public static async Task<IEnumerable<T>> SelectAsync<T>(string sqlQuery, DynamicParameters parameters)
+		{
+			using (var database = new SqlConnection(AppPromedicalDbContext.ConnectionString))
+			{
+				database.Open();
+				var result = await database.QueryAsync<T>(sqlQuery, parameters, commandType: CommandType.StoredProcedure);
+				database.Close();
+				database.Dispose();
+				//if (result == null && result.Count() > 0)
+				if (Affected(result))
+					return null;
 
-                return result;
-                //answer.ErrorGeneral = error.Message;
-                //answer.ErrorDetails = error.ToString();
-            }
-        }
+				return result;
+				//answer.ErrorGeneral = error.Message;
+				//answer.ErrorDetails = error.ToString();
+			}
+		}
 
-        public static async Task<Boolean> SendAsync(string sqlQuery, DynamicParameters parameters)
-        {
-            Boolean resultSql = true;
-            using (var database = new SqlConnection(AppPromedicalDbContext.ConnectionString))
-            {
-                database.Open();
-                var result = await database.ExecuteAsync(sqlQuery, parameters, commandType: CommandType.StoredProcedure);
-                database.Close();
-                database.Dispose();
-                if (Affected(result))
-                    return true;
+		public static async Task<Boolean> SendAsync(string sqlQuery, DynamicParameters parameters)
+		{
+			Boolean resultSql = true;
+			using (var database = new SqlConnection(AppPromedicalDbContext.ConnectionString))
+			{
+				database.Open();
+				var result = await database.ExecuteAsync(sqlQuery, parameters, commandType: CommandType.StoredProcedure);
+				database.Close();
+				database.Dispose();
+				if (Affected(result))
+					return true;
 
-                return false;
-            }
-        }
+				return false;
+			}
+		}
 
-        public static async Task<T> FindAsync<T>(string sqlQuery, DynamicParameters parameters)
-        {
-            using (var database = new SqlConnection(AppPromedicalDbContext.ConnectionString))
-            {
-                var result = await database.QueryFirstOrDefaultAsync<T>(sqlQuery, parameters, commandType: CommandType.StoredProcedure);
-                database.Close();
-                database.Dispose();
-                if (Affected(result))
-                    return default(T);
+		public static async Task<T> FindAsync<T>(string sqlQuery, DynamicParameters parameters)
+		{
+			using (var database = new SqlConnection(AppPromedicalDbContext.ConnectionString))
+			{
+				var result = await database.QueryFirstOrDefaultAsync<T>(sqlQuery, parameters, commandType: CommandType.StoredProcedure);
+				database.Close();
+				database.Dispose();
+				if (Affected(result))
+					return default(T);
 
-                return result;
-            }
-        }
+				return result;
+			}
+		}
 
-        protected static async Task<Boolean> DeleteAsync(string sqlQuery, DynamicParameters parameters)
-        {
-            using (var database = new SqlConnection(AppPromedicalDbContext.ConnectionString))
-            {
-                database.Open();
-                int result = await database.ExecuteAsync(sqlQuery, parameters, commandType: CommandType.StoredProcedure);
-                database.Close();
-                database.Dispose();
-                if (Affected(result))
-                    return true;
+		public static async Task<Boolean> DeleteAsync(string sqlQuery, DynamicParameters parameters)
+		{
+			using (var database = new SqlConnection(AppPromedicalDbContext.ConnectionString))
+			{
+				database.Open();
+				int result = await database.ExecuteAsync(sqlQuery, parameters, commandType: CommandType.StoredProcedure);
+				database.Close();
+				database.Dispose();
+				if (Affected(result))
+					return true;
 
-                return false;
-            }
-        }
+				return false;
+			}
+		}
 
-        /// <summary>
-        /// Indica si se afecto una row en la base de datos.
-        /// </summary>
-        /// <param name="result">Valor del resultado obtenido del Execute.</param>
-        /// <returns>
-        /// true si el valor indicado coincide con una row afectada.
-        /// </returns>
-        private static Boolean Affected(int result)
-        {
-            if (!result.Equals(0))
-                return false;
+		/// <summary>
+		/// Indica si se afecto una row en la base de datos.
+		/// </summary>
+		/// <param name="result">Valor del resultado obtenido del Execute.</param>
+		/// <returns>
+		/// true si el valor indicado coincide con una row afectada.
+		/// </returns>
+		private static Boolean Affected(int result)
+		{
+			if (!result.Equals(0))
+				return false;
 
-            return true;
-        }
+			return true;
+		}
 
-        /// <summary>
-        /// Indica si se afecto una row en la base de datos.
-        /// </summary>
-        /// <param name="result">Valor del resultado obtenido del QueryFirstOrDefault.</param>
-        /// <returns>
-        /// true si el valor indicado coincide con una row afectada.
-        /// </returns>
-        private static Boolean Affected(object result)
-        {
-            //result == null && result.Count() > 0
-            if (result != null)
-                return false;
+		/// <summary>
+		/// Indica si se afecto una row en la base de datos.
+		/// </summary>
+		/// <param name="result">Valor del resultado obtenido del QueryFirstOrDefault.</param>
+		/// <returns>
+		/// true si el valor indicado coincide con una row afectada.
+		/// </returns>
+		private static Boolean Affected(object result)
+		{
+			//result == null && result.Count() > 0
+			if (result != null)
+				return false;
 
-            return true;
-        }
-    }
+			return true;
+		}
+	}
 }
